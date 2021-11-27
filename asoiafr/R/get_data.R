3829bc8f89a29d5428ee99b192d45166171aba8f
payam <payamase@gmail.com>
2021-11-26 23:32:19 -0800

    initial commit for asoiafr

---
+++ asoiafr/R/get_data.R
@@ -0,0 +1,84 @@
+#' Get Data
+#'
+#' @description
+#' The \code{get_data()} function is used to get structured data from the
+#' \href{https://anapioficeandfire.com/Documentation}{"An API of Ice And Fire"} API service.
+#'
+#' @details
+#' By default, only 10 results are returned. This value can be changed using the optional
+#' \code{"pageSize"} parameter. Additionally, to specify which page to access, the optional
+#' \code{"page"} parameter can be used.
+#'
+#' @param x API resource to call.
+#' Options are \code{"books"}, \code{"characters"}, or \code{"houses"}.
+#' @param return_as_tibble When \code{TRUE}, the output will be a tibble (default); otherwise,
+#' a list will be returned.
+#' @param ... Any other optional \href{https://anapioficeandfire.com/Documentation}{parameter}
+#' that can be provided.
+#'
+#' @return An object of data for the resource parameter that was requested.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' all_books <- get_data("books", pageSize = 20)
+#' houses_5 <- get_data("houses", page = 2, pageSize = 5)
+#' characters_10 <- get_data("characters")
+#' }
+#' @importFrom dplyr mutate pull
+#' @importFrom glue glue
+#' @importFrom httr modify_url GET content http_type http_error status_code
+#' @importFrom jsonlite fromJSON
+#' @importFrom rlang arg_match
+#' @importFrom tibble as_tibble enframe
+get_data <- function(x, return_as_tibble = TRUE, ...) {
+  params <- list(...)
+
+  # Validate input ---------------------------------------------------------------------------------
+  rlang::arg_match(
+    arg = x,
+    values = c("books", "characters", "houses")
+  )
+
+  # A boolean value is required for "return_as_tibble" parameter
+  if (!is.logical(return_as_tibble)) {
+    stop("A TRUE or FALSE value is required for 'return_as_tibble' parameter")
+  }
+
+  # Numeric values should be provided for "page" and "pageSize"
+  if (!is.null(params$page)) {
+    fxn_check_page(params$page)
+  }
+  if (!is.null(params$pageSize)) {
+    fxn_check_page(params$pageSize)
+  }
+
+  # Create endpoint --------------------------------------------------------------------------------
+  path_component <- paste0("/api/", x)
+  param_components <- fxn_get_params(params)
+
+  url <- httr::modify_url(
+    url = "https://www.anapioficeandfire.com/",
+    path = path_component,
+    query = param_components
+  )
+
+  # GET response -----------------------------------------------------------------------------------
+  resp <- httr::GET(url)
+
+  # Verify response
+  fxn_check_api_return(resp)
+
+  # Parse result -----------------------------------------------------------------------------------
+  parsed <- jsonlite::fromJSON(
+    txt = httr::content(resp, "text"),
+    simplifyVector = return_as_tibble
+  )
+
+  if (return_as_tibble) {
+    parsed <- tibble::as_tibble(parsed)
+  }
+
+  # Return result ----------------------------------------------------------------------------------
+  return(parsed)
+}
---
+++ asoiafr/R/utils.R
@@ -0,0 +1,38 @@
+utils::globalVariables(c("parsed", "name", "value", "n_v"))
+
+# Get the parameters from ellipsis (...) if they were provided
+fxn_get_params <- function(l) {
+  v <- l %>%
+    tibble::enframe() %>%
+    dplyr::mutate(n_v = paste0(name, "=", value)) %>%
+    dplyr::pull(n_v) %>%
+    paste0(collapse = "&")
+  return(v)
+}
+
+# Ensure the "page" and "pageSize" parameters are numeric
+fxn_check_page <- function(x) {
+  if (!is.numeric(x) & !is.na(x)) {
+    stop(glue::glue("Numeric value needs to be provided for 'page' and 'pageSize'"))
+  }
+}
+
+# Return the error if the API call fail
+fxn_check_api_return <- function(x) {
+  if (httr::http_type(x) != "application/json") {
+    stop("API did not return json", call. = FALSE)
+  }
+
+  # Return the error if API call fails
+  if (httr::http_error(x)) {
+    stop(
+      sprintf(
+        "API request failed [%s]\n%s\n<%s>",
+        httr::status_code(x),
+        parsed$message,
+        parsed$documentation_url
+      ),
+      call. = FALSE
+    )
+  }
+}
